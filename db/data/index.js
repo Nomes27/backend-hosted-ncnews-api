55182b6cb6efb5de2fee55c93a54ffbb8bd76871
Nomes27 <nomi_smith18@hotmail.com>
2020-10-07 20:34:06 +0100

    start script is in package json

--- db/data/index.js
+++ db/data/index.js
@@ -2,6 +2,7 @@ const ENV = process.env.NODE_ENV || "development";
 const data = {
   test: require("./test-data/index"),
   development: require("./development-data/index"),
+  production: require("./development-data/index"),
 };
 
 module.exports = data[ENV];
